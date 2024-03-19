026e12df745a54f68df9384a1fc8a05f4ef0c01d
Esteban Moya <moyamena@gmail.com>
2024-03-18 21:30:19 -0300

    terminado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,37 @@
-<script>
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();   
+/*tooltip*/
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+/*smooth scroll*/
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.nav-link').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                targetSection.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
-</script>
+
+window.onscroll = () => scrollNavbar();
+
+const scrollNavbar = () =>{
+    
+    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200){
+        console.log('scroll 1')
+        document.getElementById("navbar").style.backgroundColor ="#00000060";
+    }else{
+        document.getElementById("navbar").style.backgroundColor ="transparent";
+        console.log('scroll2')
+    }
+}
+
+
